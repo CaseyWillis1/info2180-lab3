dd794924f35446c8469b890658f9256f9dae19c4
CaseyWillis1 <caseywillis140@gmail.com>
2021-10-10 18:16:40 -0500

    Added board layout

---
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -0,0 +1,14 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+    let boxes = document.querySelectorAll('#board > div');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].className = "square";
+
+    }
+
+
+});
