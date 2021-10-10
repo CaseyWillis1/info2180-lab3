b65ba16789ef623e1be2b7123b34a885fd68477c
CaseyWillis1 <caseywillis140@gmail.com>
2021-10-10 18:20:34 -0500

    Added X and O

--- tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
+    var currentplayer = 1;
 
     let boxes = document.querySelectorAll('#board > div');
     for (let i = 0; i < boxes.length; i++) {
@@ -10,5 +10,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+
+
+    boxes.forEach(element => { element.addEventListener('click', isClicked) })
+
+    function isClicked(e) {
+        if (currentplayer == 1) {
+            const box = e.target;
+            box.textContent = 'X';
+            box.className = "square X";
+            currentplayer = 0;
+
+        } else {
+            const box = e.target;
+            box.textContent = 'O';
+            box.className = ("square O");
+            currentplayer = 1;
+
+        }
+
+
+    }
+    document.querySelectorAll('square').forEach(box => box.addEventListener('click', isClicked));
 
 });
